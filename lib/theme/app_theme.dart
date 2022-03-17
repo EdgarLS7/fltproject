be3b867cbf4b82f71d13ed5c5aa8c66881e534bd
EDGAR LOPEZ SOSA <16171409@itculiacan.edu.mx>
2022-03-17 00:25:27 -0700

    terminado 77

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
 
-  static const Color primary = Colors.indigo;
+  static const Color primary = Colors.orange;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     //Color primario
