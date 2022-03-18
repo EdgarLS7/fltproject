d50fe38a19cf45a16454ac2bf3fbb27e1b587429
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-03-17 17:05:34 -0700

    saber en que plataforma esta el telefono para la alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,10 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog( BuildContext context ) {
+  void displayDialogIOS( BuildContext context ) {
+    
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: ( context ) {
+        return CupertinoAlertDialog(
+          title: const Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox( height: 10,),
+              FlutterLogo( size: 70,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cerrar', style: TextStyle( color: Colors.red),)
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok')
+            ),
+          ],
+        );
+      }
+    );
+  } 
+
+  void displayDialogAndroid( BuildContext context ) {
 
     showDialog(
       barrierDismissible: false,
@@ -26,7 +60,11 @@ class AlertScreen extends StatelessWidget {
             TextButton(
               onPressed: () => Navigator.pop(context),
               child: const Text('Cerrar')
-            )
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok')
+            ),
           ],
         );
       }
@@ -45,7 +83,10 @@ class AlertScreen extends StatelessWidget {
              padding: EdgeInsets.all(10),
              child: Text('Mostrar Alerta', style: TextStyle( fontSize: 18),),
            ),
-           onPressed: () => displayDialog(context),
+          //  onPressed: () => displayDialogAndroid(context),
+          onPressed: () => Platform.isAndroid 
+            ? displayDialogAndroid(context)
+            : displayDialogIOS(context)
          )
       ),
       floatingActionButton: FloatingActionButton(
